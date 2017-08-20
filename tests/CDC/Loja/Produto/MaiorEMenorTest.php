6276c8bc3334e65e9efeb3611bc59490b24507c3
Henrique Rodrigues <henoliv@gmail.com>
2017-08-20 11:03:12 -0300

    Testando apenas um produto

--- tests/CDC/Loja/Produto/MaiorEMenorTest.php
+++ tests/CDC/Loja/Produto/MaiorEMenorTest.php
@@ -35,4 +35,25 @@ class MaiorEMenorTest extends PHPUnit
             $maiorMenor->getMaior()->getNome()
         );
     }
+
+    public function testApenasUmProduto()
+    {
+        $carrinho = new CarrinhoDeCompras();
+        
+        $carrinho->adiciona(new Produto("Geladeira", 450.00));
+
+        $maiorMenor = new MaiorEMenor();
+        
+        $maiorMenor->encontra($carrinho);
+
+        $this->assertEquals(
+            "Geladeira",
+            $maiorMenor->getMenor()->getNome()
+        );
+
+        $this->assertEquals(
+            "Geladeira",
+            $maiorMenor->getMaior()->getNome()
+        );
+    }
 }
