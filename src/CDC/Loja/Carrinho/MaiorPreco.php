41b581efe976f730df8ea8394c24d70d0832eece
Henrique Rodrigues <henrique@hostnet.com.br>
2017-08-24 14:40:25 -0300

    Testes de produto de maior valor

--- src/CDC/Loja/Carrinho/MaiorPreco.php
+++ src/CDC/Loja/Carrinho/MaiorPreco.php
@@ -8,6 +8,18 @@ class MaiorPreco
 {
     public function encontra(CarrinhoDeCompras $carrinho)
     {
-        return 0;
+        if (count($carrinho->getProdutos()) === 0) {
+            return 0;
+        }
+        
+        $maiorValor = $carrinho->getProdutos()[0]->getValorTotal();
+        
+        foreach ($carrinho->getProdutos() as $produto) {
+            $maiorValor = $produto->getValorTotal() > $maiorValor
+                ? $produto->getValorTotal()
+                : $maiorValor;
+        }
+        
+        return $maiorValor;
     }
 }
--- tests/CDC/Loja/Carrinho/MaiorPrecoTest.php
+++ tests/CDC/Loja/Carrinho/MaiorPrecoTest.php
@@ -5,6 +5,7 @@ namespace CDC\Loja\Carrinho;
 use CDC\Loja\Test\TestCase;
 use CDC\Loja\Carrinho\CarrinhoDeCompras;
 use CDC\Loja\Carrinho\MaiorPreco;
+use CDC\Loja\Produto\Produto;
 
 class MaiorPrecoTest extends TestCase
 {
@@ -17,5 +18,33 @@ class MaiorPrecoTest extends TestCase
         $valor = $algoritmo->encontra($carrinho);
 
         $this->assertEquals(0, $valor, null, 0.0001);
+    }
+
+    public function testDeveRetornarValorDoItemSeCarrinhoCom1Elemento()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->adiciona(new Produto("Geladeira", 900.00, 1));
+
+        $algoritmo = new MaiorPreco();
+
+        $valor = $algoritmo->encontra($carrinho);
+
+        $this->assertEquals(900.00, $valor, null, 0.0001);
+    }
+
+    public function testDeveRetornarValorDoItemSeCarrinhoComMuitosElementos()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->adiciona(new Produto("Geladeira", 900.00, 1));
+        $carrinho->adiciona(new Produto("Fogão", 1500.00, 1));
+        $carrinho->adiciona(new Produto("Máquina de lavar", 750.00, 1));
+
+        $algoritmo = new MaiorPreco();
+
+        $valor = $algoritmo->encontra($carrinho);
+
+        $this->assertEquals(1500.00, $valor, null, 0.0001);
     }
 }
