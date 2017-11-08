b08a4ff064825a67419b3e571b8c1780b346c4e8
Hostnet Dev <henrique@hostnet.com.br>
2017-11-08 12:00:35 -0200

    Seguindo princípio de Demeter

--- src/CDC/Loja/FluxoDeCaixa/Fatura.php
+++ src/CDC/Loja/FluxoDeCaixa/Fatura.php
@@ -16,6 +16,7 @@ class Fatura
         $this->cliente = $cliente;
         $this->valor = $valor;
         $this->pagamentos = new ArrayObject();
+        $this->pago = false;
     }
 
     public function getCliente(): string
@@ -41,5 +42,20 @@ class Fatura
     public function isPago(): bool
     {
         return $this->pago;
+    }
+
+    public function adicionaPagamento(Pagamento $pagamento)
+    {
+        $this->pagamentos->append($pagamento);
+
+        $valorTotal = 0;
+        
+        foreach ($this->pagamentos as $p) {
+            $valorTotal += $p->getValor();
+        }
+
+        if ($valorTotal >= $this->valor) {
+            $this->pago = true;
+        }
     }
 }
--- src/CDC/Loja/FluxoDeCaixa/ProcessadorDeBoletos.php
+++ src/CDC/Loja/FluxoDeCaixa/ProcessadorDeBoletos.php
@@ -12,21 +12,14 @@ class ProcessadorDeBoletos
     public function processa(ArrayObject $boletos, Fatura $fatura)
     {
         $pagamentosFatura = $fatura->getPagamentos();
-        $totalPagamento = 0;
         
         foreach ($boletos as $boleto) {
             $pagamento = new Pagamento(
                 $boleto->getValor(),
                 MeioPagamento::BOLETO
             );
-            
-            $pagamentosFatura->append($pagamento);
-            
-            $totalPagamento += $boleto->getValor();
-        }
 
-        $fatura->setPago(
-            $totalPagamento >= $fatura->getValor() ? true : false
-        );
+            $fatura->adicionaPagamento($pagamento);
+        }
     }
 }
